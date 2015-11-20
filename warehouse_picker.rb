14d0bbfc2627041466515d66aef43e3c62c245fe
Callum Hogg <bananaCypher@gmail.com>
2015-11-20 11:51:30 +0000

    Changed the data structure to an Array of Hashes and updated the functions

--- warehouse_picker.rb
+++ warehouse_picker.rb
@@ -1,46 +1,214 @@
-BAYS = {
-  'b7' => 'bath fizzers',
-  'a3' => 'blouse',
-  'a7' => 'bookmark',
-  'c8' => 'candy wrapper',
-  'c3' => 'chalk',
-  'b10' => 'cookie jar',
-  'b9' => 'deodorant',
-  'c2' => 'drill press',
-  'c6' => 'face wash',
-  'a9' => 'glow stick',
-  'a4' => 'hanger',
-  'c10' => 'leg warmers',
-  'a8' => 'model car',
-  'b5' => 'nail filer',
-  'a1' => 'needle',
-  'c7' => 'paint brush',
-  'b4' => 'photo album',
-  'b3' => 'picture frame',
-  'a10' => 'rubber band',
-  'a5' => 'rubber duck',
-  'c1' => 'rusty nail',
-  'b2' => 'sharpie',
-  'c9' => 'shoe lace',
-  'a6' => 'shovel',
-  'a2' => 'stop sign',
-  'c5' => 'thermometer',
-  'b1' => 'tyre swing',
-  'b8' => 'tissue box',
-  'b6' => 'tooth paste',
-  'c4' => 'word search'
-}
+require 'pry-byebug'
 
-def get_item_from_bay(bay)
-  BAYS[bay]
+BAYS = [
+  {
+    'bay' => 'bath fizzers',
+    'item' => 'bath fizzers',
+    'bay_letter' => 'b',
+    'bay_number' => 7
+  },
+  {
+    'bay' => 'a3',
+    'item' => 'blouse',
+    'bay_letter' => 'a',
+    'bay_number' => 3
+  },
+  {
+    'bay' => 'a7',
+    'item' => 'bookmark',
+    'bay_letter' => 'a',
+    'bay_number' => 7
+  },
+  {
+    'bay' => 'c8',
+    'item' => 'candy wrapper',
+    'bay_letter' => 'c',
+    'bay_number' => 8
+  },
+  {
+    'bay' => 'c3',
+    'item' => 'chalk',
+    'bay_letter' => 'c',
+    'bay_number' => 3
+  },
+  {
+    'bay' => 'b10',
+    'item' => 'cookie jar',
+    'bay_letter' => 'b',
+    'bay_number' => 10
+  },
+  {
+    'bay' => 'b9',
+    'item' => 'deodorant',
+    'bay_letter' => 'b',
+    'bay_number' => 9
+  },
+  {
+    'bay' => 'c2',
+    'item' => 'drill press',
+    'bay_letter' => 'c',
+    'bay_number' => 2
+  },
+  {
+    'bay' => 'c6',
+    'item' => 'face wash',
+    'bay_letter' => 'c',
+    'bay_number' => 6
+  },
+  {
+    'bay' => 'a9',
+    'item' => 'glow stick',
+    'bay_letter' => 'a',
+    'bay_number' => 9
+  },
+  {
+    'bay' => 'a4',
+    'item' => 'hanger',
+    'bay_letter' => 'a',
+    'bay_number' => 4
+  },
+  {
+    'bay' => 'c10',
+    'item' => 'leg warmers',
+    'bay_letter' => 'c',
+    'bay_number' => 10
+  },
+  {
+    'bay' => 'a8',
+    'item' => 'model car',
+    'bay_letter' => 'a',
+    'bay_number' => 8
+  },
+  {
+    'bay' => 'b5',
+    'item' => 'nail filer',
+    'bay_letter' => 'b',
+    'bay_number' => 5
+  },
+  {
+    'bay' => 'a1',
+    'item' => 'needle',
+    'bay_letter' => 'a',
+    'bay_number' => 1
+  },
+  {
+    'bay' => 'c7',
+    'item' => 'paint brush',
+    'bay_letter' => 'c',
+    'bay_number' => 7
+  },
+  {
+    'bay' => 'b4',
+    'item' => 'photo album',
+    'bay_letter' => 'b',
+    'bay_number' => 4
+  },
+  {
+    'bay' => 'b3',
+    'item' => 'picture frame',
+    'bay_letter' => 'b',
+    'bay_number' => 3
+  },
+  {
+    'bay' => 'a10',
+    'item' => 'rubber band',
+    'bay_letter' => 'a',
+    'bay_number' => 10
+  },
+  {
+    'bay' => 'a5',
+    'item' => 'rubber duck',
+    'bay_letter' => 'a',
+    'bay_number' => 5
+  },
+  {
+    'bay' => 'c1',
+    'item' => 'rusty nail',
+    'bay_letter' => 'c',
+    'bay_number' => 1
+  },
+  {
+    'bay' => 'b2',
+    'item' => 'sharpie',
+    'bay_letter' => 'b',
+    'bay_number' => 2
+  },
+  {
+    'bay' => 'c9',
+    'item' => 'shoe lace',
+    'bay_letter' => 'c',
+    'bay_number' => 9
+  },
+  {
+    'bay' => 'a6',
+    'item' => 'shovel',
+    'bay_letter' => 'a',
+    'bay_number' => 6
+  },
+  {
+    'bay' => 'a2',
+    'item' => 'stop sign',
+    'bay_letter' => 'a',
+    'bay_number' => 2
+  },
+  {
+    'bay' => 'c5',
+    'item' => 'thermometer',
+    'bay_letter' => 'c',
+    'bay_number' => 5
+  },
+  {
+    'bay' => 'b1',
+    'item' => 'tyre sing',
+    'bay_letter' => 'b',
+    'bay_number' => 1
+  },
+  {
+    'bay' => 'b8',
+    'item' => 'tissue box',
+    'bay_letter' => 'b',
+    'bay_number' => 8
+  },
+  {
+    'bay' => 'b6',
+    'item' => 'tooth paste',
+    'bay_letter' => 'b',
+    'bay_number' => 6
+  },
+  {
+    'bay' => 'c4',
+    'item' => 'word search',
+    'bay_letter' => 'c',
+    'bay_number' => 4
+  },
+]
+
+def get_item_from_bay(bay_key)
+  BAYS.detect{|bay| bay_key == bay['bay']}['item']
 end
 
 def get_bay_by_item(item)
-  BAYS.each {|bay, bay_item| return bay if bay_item.downcase == item.downcase}
+  BAYS.each {|bay| return bay['bay'] if bay['item'] == item}
 end
 
-def get_item_from_bay_list(bay_list)
+def get_items_from_bay_list(bay_list)
   item_list = []
   bay_list.each {|bay| item_list.push(get_item_from_bay(bay))}
   item_list
+end
+
+def get_bays_from_item_list(item_list)
+  bay_list = []
+  item_list.each {|item| bay_list.push(get_bay_by_item(item))}
+  bay_list
+end
+
+def get_bay_value(bay)
+  bay[1,test.length].to_i
+end
+
+def get_items_from_bay_list_with_distance(bay_list)
+  bays = get_items_from_bay_list(bay_list)
+  bay_values = bays.map {|bay| get_bay_value(bay)}
+  binding.pry
 end
--- warehouse_picker_runner.rb
+++ warehouse_picker_runner.rb
@@ -9,7 +9,21 @@ item_bay = get_bay_by_item('nail filer')
 puts "The item nail filer can be found in bay #{item_bay}, expect it to be b5"
 
 # Given a list of bays, list the items in those bays
-item_list = get_item_from_bay_list(['b5', 'b10', 'b6'])
+item_list = get_items_from_bay_list(['b5', 'b10', 'b6'])
 puts "The items in bays b5, b10 and b6 are:- "
 item_list.each {|item| puts item}
 puts "Should be nail filer, cookie jar, and tooth paste"
+
+# Given a list of items find the bays.
+bay_list = get_bays_from_item_list(['shoe lace', 'rusty nail', 'leg warmers'])
+puts "The bays that contain shoe lace, rusty nail and leg warmers are:-"
+bay_list.each {|bay| puts bay}
+puts "Should be c1, c9 and c10"
+
+# #Given a list of bays, list the items in those bays, and calculate the distance from the two furthest apart bays. 
+# items_and_distance = get_items_from_bay_list_with_distance(['b5', 'b10', 'b6'])
+# puts "The items in bays b5, b10 and b6 are:-"
+# items_and_distance[0].each {|item| puts item}
+# puts "Should be nail filer, cookie jar and tooth paste.  And the distance between them is #{items_and_distance[1]}, should be 5"
+
+# given "b3, c7, c9 and a3", determine that the products are "picture frame, paint brush, shoe lace, and blouse", and they're 15 bays apart.)
